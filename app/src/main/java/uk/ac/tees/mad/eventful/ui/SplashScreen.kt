fa60151a57288bcd2efa5119d7254556509a247f
Akhil-dev300 <akhil.s0503@gmail.com>
2024-11-22 18:13:58 +0000

    Commit 2
    Commit 2

--- app/src/main/java/uk/ac/tees/mad/eventful/ui/SplashScreen.kt
+++ app/src/main/java/uk/ac/tees/mad/eventful/ui/SplashScreen.kt
@@ -1,10 +1,60 @@
 package uk.ac.tees.mad.eventful.ui
 
-import androidx.compose.material3.Text
+import android.view.animation.OvershootInterpolator
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavHostController
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import coil.compose.AsyncImage
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
+import uk.ac.tees.mad.eventful.R
 
 @Composable
-fun SplashScreen(navController: NavHostController) {
-    Text(text = "Splash screen")
+fun SplashScreen(navController: NavController) {
+    // Animation state for logo scaling
+    val scale = remember { Animatable(0f) }
+
+    LaunchedEffect(Unit) {
+        scale.animateTo(
+            targetValue = 1f,
+            animationSpec = tween(
+                durationMillis = 1000,
+                easing = { OvershootInterpolator(2f).getInterpolation(it) }
+            )
+        )
+
+        delay(2000)
+        withContext(Dispatchers.Main) {
+            navController.navigate("login") {
+                popUpTo("splash") { inclusive = true }
+            }
+        }
+    }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .clip(RoundedCornerShape(40.dp)),
+        contentAlignment = Alignment.Center
+    ) {
+        AsyncImage(
+            model = R.drawable.event,
+            contentDescription = "logo",
+            modifier = Modifier
+                .clip(RoundedCornerShape(40.dp))
+                .scale(scale.value)
+        )
+    }
 }
